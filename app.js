8d6c566e46964ea83dc1685055e37c7f5aa688bc
Syed Shahwaiz Qasim <syedshahwaiz32@gmail.com>
2024-06-01 22:49:16 -0700

    50% styling complete

--- app.js
+++ app.js
@@ -4,11 +4,12 @@ const GithubUserName = document.querySelector("#UserName");
 const UserBio = document.querySelector("#UserBio");
 
 const formController = async () => {
-const api1 = await fetch("https://api.github.com/users/shehza-d");
+const api1 = await fetch("https://api.github.com/users/mkaif5");
 const response = await api1.json();
 
-GithubImage.setAttribute("src", response.avatar_url);
-GithubUserName.innerText = response.bio;
+GithubImage.src = response.avatar_url;
+GithubUserName.innerText = response.name;
+UserBio.innerText = response.bio;
 console.log(response);
 }
 formController();
