4ece37a2d34086fe133fe59e0a81673445a19e18
Syed Shahwaiz Qasim <syedshahwaiz32@gmail.com>
2024-06-03 07:24:51 -0700

    styling and script add

--- app.js
+++ app.js
@@ -1,18 +1,25 @@
 const GithubForm = document.querySelector("#myForm");
+const UserInput = document.querySelector("#GithubUser");
 const GithubImage = document.querySelector("#UserImage");
+const GithubName = document.querySelector("#GithubName");
 const GithubUserName = document.querySelector("#UserName");
 const UserBio = document.querySelector("#UserBio");
 
-const formController = async () => {
-const api1 = await fetch("https://api.github.com/users/mkaif5");
+const formController = async (event) => {
+event.preventDefault();
+
+const GithubUserInput = UserInput.value;
+// console.log(GithubUserInput);
+
+const api1 = await fetch(`https://api.github.com/users/${GithubUserInput}`);
 const response = await api1.json();
 
 GithubImage.src = response.avatar_url;
 GithubUserName.innerText = response.name;
 UserBio.innerText = response.bio;
-console.log(response);
+//console.log(response);
 }
-formController();
+// formController();
 
-GithubForm.addEventListener("submit", formController)
+GithubForm.addEventListener("submit", formController);
 
