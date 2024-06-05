612b6e56b173150e59f5a7745feb462f6121b8bb
Syed Shahwaiz Qasim <syedshahwaiz32@gmail.com>
2024-06-05 07:21:33 -0700

    css add

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ const UserInput = document.querySelector("#GithubUser");
 const GithubImage = document.querySelector("#UserImage");
 const GithubName = document.querySelector("#GithubName");
 const GithubUserName = document.querySelector("#UserName");
+const GithubJoin = document.querySelector("GithubJoin");
 const UserBio = document.querySelector("#UserBio");
+const userCity = document.querySelector("#UserCity");
 
 const formController = async (event) => {
 event.preventDefault();
@@ -13,11 +15,12 @@ const GithubUserInput = UserInput.value;
 
 const api1 = await fetch(`https://api.github.com/users/${GithubUserInput}`);
 const response = await api1.json();
+console.log(response);
 
 GithubImage.src = response.avatar_url;
-GithubUserName.innerText = response.name;
+GithubName.innerText = response.name;
+GithubUserName.innerText = `@${response.login}`;
 UserBio.innerText = response.bio;
-//console.log(response);
 }
 // formController();
 
